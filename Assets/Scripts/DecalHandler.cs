e93d3a5014a6bcc150f5facaa2d316768cf7d1b7
Lemonoak <45938598+Lemonoak@users.noreply.github.com>
2019-04-03 12:21:33 +0200

    Fixed instantiate rotation
    Fixed instantiate rotation

---
+++ Assets/Scripts/DecalHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecalHandler : MonoBehaviour
+{
+
+    public GameObject Decal;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GetPosition();
+        }
+    }
+
+    void GetPosition()
+    {
+        RaycastHit RayHit;
+        Ray Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(Ray, out RayHit))
+        {
+            //Debug.Log(RayHit.point);
+            //Debug.Log(RayHit.normal);
+            Quaternion Rot = Quaternion.FromToRotation(Vector3.up, RayHit.normal);
+            //Debug.Log(Rot);
+            Instantiate(Decal, RayHit.point + (RayHit.normal * 0.001f), Rot);
+        }
+    }
+}
