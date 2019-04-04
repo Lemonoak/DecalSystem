ffaf754dd08feade548e95233148d58cdd1eeba8
Lemonoak <45938598+Lemonoak@users.noreply.github.com>
2019-04-04 15:35:14 +0200

    Added Decal Mesh, fixed decal layermask
    Added Decal Mesh, fixed decal layermask

--- Assets/Scripts/DecalHandler.cs
+++ Assets/Scripts/DecalHandler.cs
@@ -1,16 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class DecalHandler : MonoBehaviour
 {
-
+    //The Decal to spawn
     public GameObject Decal;
-
-    void Start()
-    {
-        
-    }
+    public DecalMesh Mesh;
 
     void Update()
     {
@@ -22,17 +16,18 @@ public class DecalHandler : MonoBehaviour
 
     void GetPosition()
     {
-        //LayerMask LayersToIgnore;
-        //LayersToIgnore =~ LayerMask.GetMask("Decal");
+        LayerMask LayersToIgnore = LayerMask.GetMask("Decal");
         RaycastHit RayHit;
         Ray Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(Ray, out RayHit/*, LayersToIgnore*/))
+        if(Physics.Raycast(Ray, out RayHit, LayersToIgnore))
         {
             //Debug.Log(RayHit.point);
             //Debug.Log(RayHit.normal);
             Quaternion Rot = Quaternion.FromToRotation(Vector3.up, RayHit.normal);
             //Debug.Log(Rot);
+            //Debug.Log(LayersToIgnore);
             Instantiate(Decal, RayHit.point + (RayHit.normal * 0.001f), Rot);
+            //Graphics.DrawMeshInstanced();
         }
     }
 }
---
+++ Assets/Scripts/DecalMesh.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class DecalMesh : MonoBehaviour
+{
+
+    public Material MeshMaterial;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+
+        }
+    }
+
+    public void SpawnMesh()
+    {
+        //The Gameobject
+        GameObject MyPlane = new GameObject("Plane");
+        //Adds a meshfilter to the gameobject
+        MeshFilter MeshFilter = (MeshFilter)MyPlane.AddComponent(typeof(MeshFilter));
+        //Size of the gameobject mesh
+        MeshFilter.mesh = MyMesh(0.1f, 0.1f);
+        //Adds a mesh renderer to the object
+        MeshRenderer MyMeshRenderer = MyPlane.AddComponent<MeshRenderer>();
+        MyMeshRenderer.material.color = Color.red;
+        MyMeshRenderer.material = MeshMaterial;
+    }
+
+    Mesh MyMesh(float Width, float Height)
+    {
+        Mesh ThisMesh = new Mesh();
+        ThisMesh.name = "MeshByCode";
+        ThisMesh.vertices = new Vector3[]
+        {
+            //Bottom Left Corner
+            new Vector3(-Width, -Height, 0.01f),
+            //Bottom, Right Corner
+            new Vector3(Width, -Height, 0.01f),
+            //Top Right Corner
+            new Vector3(Width, Height, 0.01f),
+            //Top Left Corner
+            new Vector3(-Width, Height, 0.01f)
+        };
+        ThisMesh.uv = new Vector2[]
+        {
+            new Vector2 (0, 0),
+            new Vector2 (0, 1),
+            new Vector2(1, 1),
+            new Vector2 (1, 0)
+        };
+        ThisMesh.triangles = new int[] { 0, 1, 2, 0, 2, 3 };
+        ThisMesh.RecalculateNormals();
+
+        return ThisMesh;
+    }
+}
